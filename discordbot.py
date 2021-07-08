f4386c3b50c1ee8c6421ca37495d032291a7949e
masyo-lab <masyo.lab@gmail.com>
2021-07-08 22:59:23 +0900

    no message

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,14 @@
+import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix='/')
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 SEND_CHANNEL = os.environ['SEND_CHANNEL']  # 送信用チャンネル
 ASSAULTLILY_LOG_CHANNEL = os.environ['ASSAULTLILY_LOG'] # レギオンマッチ ログ
 
-@bot.event
+@client.event
 # メッセージを受け取った際のイベント
 async def on_message(message):
 
@@ -17,9 +19,9 @@ async def on_message(message):
     # 送信用チャンネルかを確認
     if message.channel.id == SEND_CHANNEL:
         # 送信先チャンネルを設定
-        target_channel = bot.get_channel(ASSAULTLILY_LOG_CHANNEL)
+        target_channel = client.get_channel(ASSAULTLILY_LOG_CHANNEL)
         # 送信用チャンネルから受け取った内容を送信
         await target_channel.send(message.content)
 
 # Botの起動とDiscordサーバーへの接続
-bot.run(token)
+client.run(token)
