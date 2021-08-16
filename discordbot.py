ac2493314087720d676baf5c8fa0c68a2f2b7e75
masyo-lab <masyo.lab@gmail.com>
2021-08-17 02:35:34 +0900

    client に変更

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,8 @@
+import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
 bot_token = os.environ['DISCORD_BOT_TOKEN']
 
 # レギオンマッチ専用
@@ -9,7 +10,7 @@ legion_match_log_input = os.environ['LEGION_MATCH_LOG_INPUT']
 legion_match_log_output = os.environ['LEGION_MATCH_LOG_OUTPUT']
 
 
-@bot.event
+@client.event
 # メッセージを受け取った際のイベント
 async def on_message(message):
 
@@ -22,8 +23,8 @@ async def on_message(message):
     # レギオンマッチ専用
     if message.channel.id == legion_match_log_input:
         # 送信先チャンネルを設定
-        target_channel = bot.get_channel(legion_match_log_output)
+        target_channel = client.get_channel(legion_match_log_output)
         # 送信用チャンネルから受け取った内容を送信
         await target_channel.send(message.content)
 
-bot.run(bot_token)
+client.run(bot_token)
