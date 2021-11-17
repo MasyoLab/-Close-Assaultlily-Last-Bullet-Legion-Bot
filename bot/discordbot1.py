3760adf4d6dfa54a460992a4c6f851833f4ffab8
masyo-lab <masyo.lab@gmail.com>
2021-11-18 01:05:56 +0900

    add play game

--- bot/discordbot1.py
+++ bot/discordbot1.py
@@ -5,6 +5,11 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+async def on_ready():
+    game = bot.Game('Assaultliliyをプレイ中')
+    await bot.change_presence(status=bot.Status.online, activity=game)
+
+
 def client_run():
     bot.run(token)
 
--- bot/discordbot2.py
+++ bot/discordbot2.py
@@ -5,6 +5,11 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN_2']
 
 
+async def on_ready():
+    game = bot.Game('CHARMの妖精')
+    await bot.change_presence(status=bot.Status.online, activity=game)
+
+
 def client_run():
     bot.run(token)
 
