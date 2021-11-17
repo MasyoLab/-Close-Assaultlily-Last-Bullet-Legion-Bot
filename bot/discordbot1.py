853e9e0d858212de05976b02e615f83e0b2e5d95
masyo-lab <masyo.lab@gmail.com>
2021-11-18 08:19:03 +0900

    delete time

--- bot/discordbot1.py
+++ bot/discordbot1.py
@@ -1,9 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-import datetime
-
-DIFF_JST_FROM_UTC = 9
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -11,9 +8,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
 async def on_ready():
-    startTime = datetime.timedelta(2021, 1, 20)
-    nowTime = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
-    game = discord.Game(name='Assaultliliy', start=startTime, end=nowTime)
+    game = discord.Game(name='Assaultliliy')
     await bot.change_presence(status=discord.Status.online, activity=game)
 
 
--- bot/discordbot2.py
+++ bot/discordbot2.py
@@ -1,9 +1,6 @@
 import discord
 from discord.ext import commands
 import os
-import datetime
-
-DIFF_JST_FROM_UTC = 9
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN_2']
@@ -11,9 +8,7 @@ token = os.environ['DISCORD_BOT_TOKEN_2']
 
 @bot.event
 async def on_ready():
-    startTime = datetime.timedelta(2021, 1, 20)
-    nowTime = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
-    game = discord.Game(name='CHARMの妖精', start=startTime, end=nowTime)
+    game = discord.Game(name='CHARMの妖精')
     await bot.change_presence(status=discord.Status.online, activity=game)
 
 
