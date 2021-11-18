892a97fbe673c512b8f7b01b6827bc50ff05d166
masyo-lab <masyo.lab@gmail.com>
2021-11-19 02:54:00 +0900

    update

--- bot/discordbot2.py
+++ bot/discordbot2.py
@@ -2,35 +2,28 @@ import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN_2']
 get_channel_id = os.environ['BOT2_GET_CHANNEL_ID']
 post_channel_id = os.environ['BOT2_POST_CHANNEL_ID']
 
 
-@bot.event
+@client.event
 async def on_ready():
     game = discord.Game(name='CHARMの妖精')
-    await bot.change_presence(status=discord.Status.online, activity=game)
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 
-# @bot.command()
-# async def on_message(message: discord.Message):
-#    bot.get_channel
-#    if message.channel == get_channel_id:
-#        target_channel = bot.get_channel(post_channel_id)
-#        await target_channel.send(message.content)
-@bot.command(pass_context=True)
-async def xsend(ctx, *, message: str):
-    await bot.delete_message(ctx.message)
-    channel = bot.get_channel(post_channel_id)
-    if channel:
-        await bot.send_message(channel, message)
+@client.event
+async def on_message(message):
+    if message.channel.id == get_channel_id:
+        target_channel = client.get_channel(post_channel_id)
+        await target_channel.send(message.content)
 
 
 def client_run():
-    bot.run(token)
+    client.run(token)
 
 
 async def async_client_run():
-    await bot.start(token)
+    await client.start(token)
