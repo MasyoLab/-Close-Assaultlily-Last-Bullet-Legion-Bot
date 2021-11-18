f1006bcc89b29281745c0a054cd76468c5f6937a
masyo-lab <masyo.lab@gmail.com>
2021-11-19 01:50:23 +0900

    add post action

--- bot/discordbot2.py
+++ bot/discordbot2.py
@@ -4,12 +4,21 @@ import os
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN_2']
+get_channel_id = os.environ['BOT2_GET_CHANNEL_ID']
+post_channel_id = os.environ['BOT2_POST_CHANNEL_ID']
 
 
 @bot.event
 async def on_ready():
     game = discord.Game(name='CHARMの妖精')
     await bot.change_presence(status=discord.Status.online, activity=game)
+
+
+@bot.event
+async def on_message(message):
+    if message.channel.id == get_channel_id:
+        target_channel = bot.get_channel(post_channel_id)
+        await target_channel.send(message.content)
 
 
 def client_run():
