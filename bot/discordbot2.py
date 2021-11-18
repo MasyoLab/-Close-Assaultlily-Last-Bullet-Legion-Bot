8cde4cc767e32cc61080be21a1b7b6c1a859513b
masyo-lab <masyo.lab@gmail.com>
2021-11-19 02:39:55 +0900

    dummy

--- bot/discordbot2.py
+++ bot/discordbot2.py
@@ -14,11 +14,18 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=game)
 
 
-@bot.command()
-async def on_message(message: discord.Message):
-    if message.channel.id == get_channel_id:
-        target_channel = bot.get_channel(post_channel_id)
-        await target_channel.send(message.content)
+# @bot.command()
+# async def on_message(message: discord.Message):
+#    bot.get_channel
+#    if message.channel == get_channel_id:
+#        target_channel = bot.get_channel(post_channel_id)
+#        await target_channel.send(message.content)
+@bot.command(pass_context=True)
+async def xsend(ctx, *, message: str):
+    await bot.delete_message(ctx.message)
+    channel = bot.get_channel(post_channel_id)
+    if channel:
+        await bot.send_message(channel, message)
 
 
 def client_run():
