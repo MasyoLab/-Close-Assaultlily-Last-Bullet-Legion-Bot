54b882705ae615f14c1318813083b5e72b26b84e
masyo-lab <masyo.lab@gmail.com>
2021-11-19 10:37:32 +0900

    no message

--- bot/discordbot2.py
+++ bot/discordbot2.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN_2']
@@ -10,6 +11,19 @@ token = os.environ['DISCORD_BOT_TOKEN_2']
 async def on_ready():
     game = discord.Game(name='CHARMの妖精')
     await bot.change_presence(status=discord.Status.online, activity=game)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(
+        traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 
 def client_run():
