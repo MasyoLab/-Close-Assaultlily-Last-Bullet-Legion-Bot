74e1737c191fadc6e4003416f779d0ba31258390
masyo-lab <masyo.lab@gmail.com>
2021-11-21 02:06:21 +0900

    Revert "delete"
    This reverts commit 4c0cca18bcf8fdc4b0e8b688fe171b1957bc6ced.

---
+++ bot/__init__.py
@@ -0,0 +1,2 @@
+from . import discordbot1
+from . import discordbot2
---
+++ bot/discordbot2.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN_2']
+
+
+@bot.event
+async def on_ready():
+    game = discord.Game(name='CHARMの妖精')
+    await bot.change_presence(status=discord.Status.online, activity=game)
+
+
+def client_run():
+    bot.run(token)
+
+
+async def async_client_run():
+    await bot.start(token)
