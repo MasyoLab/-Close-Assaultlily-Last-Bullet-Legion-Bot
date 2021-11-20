7f2c44b0ac5b9f27c5ab44ecbfef714c81b93855
masyo-lab <masyo.lab@gmail.com>
2021-11-21 00:43:16 +0900

    reset

--- bot/discordbot2.py
+++ bot/discordbot2.py
@@ -3,10 +3,7 @@ from discord.ext import commands
 import os
 
 bot = commands.Bot(command_prefix='/')
-
 token = os.environ['DISCORD_BOT_TOKEN_2']
-get_channel = os.environ['BOT2_GET_CHANNEL_ID']
-post_channel = os.environ['BOT2_POST_CHANNEL_ID']
 
 
 @bot.event
@@ -21,18 +18,3 @@ def client_run():
 
 async def async_client_run():
     await bot.start(token)
-
-
-@bot.event
-# メッセージを受け取った際のイベント
-async def on_message(message: discord.Message):
-    # 送信用チャンネルかを確認
-    if message.channel.id == get_channel:
-
-        guild: discord.Guild = message.guild()
-
-        # 送信先チャンネルを設定
-        target_channel: discord.guild.TextChannel = guild.get_channel(
-            post_channel)
-        # 送信用チャンネルから受け取った内容を送信
-        await target_channel.send(message.content)
