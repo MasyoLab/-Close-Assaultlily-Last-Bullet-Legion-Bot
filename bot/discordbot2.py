7024d667099c326fe88f4c6e69b1ea4922ccd658
masyo-lab <masyo.lab@gmail.com>
2021-11-21 00:05:09 +0900

    save

--- bot/discordbot2.py
+++ bot/discordbot2.py
@@ -1,20 +1,40 @@
 import discord
-from discord.ext import commands
+from datetime import datetime
+from discord.ext import tasks
 import os
 
-bot = commands.Bot(command_prefix='/')
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN_2']
+get_channel = os.environ['BOT2_GET_CHANNEL_ID']
+post_channel = os.environ['BOT2_POST_CHANNEL_ID']
 
 
-@bot.event
+@client.event
 async def on_ready():
     game = discord.Game(name='CHARMの妖精')
-    await bot.change_presence(status=discord.Status.online, activity=game)
+    await client.change_presence(status=discord.Status.online, activity=game)
 
 
 def client_run():
-    bot.run(token)
+    client.run(token)
 
 
 async def async_client_run():
-    await bot.start(token)
+    await client.start(token)
+
+
+@client.event
+# メッセージを受け取った際のイベント
+async def on_message(message):
+
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+
+    # 送信用チャンネルかを確認
+    if message.channel.id == get_channel:
+        # 送信先チャンネルを設定
+        target_channel = client.get_channel(post_channel)
+        # 送信用チャンネルから受け取った内容を送信
+        await target_channel.send(message.content)
